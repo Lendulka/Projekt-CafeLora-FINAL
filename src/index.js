5934b31f6653bc0b2dbe99b56d15c7897fe025b7
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-06-25 23:00:36 +0200

    Úprava routování

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const { pathname } = window.location
 
 const appElm = document.querySelector('#app')
 
-if (pathname === BASE_PATH + '/') {
+if (pathname === BASE_PATH + '/' || pathname === BASE_PATH + '/home') {
     appElm.append(HomePage())
 } else if (pathname === BASE_PATH + '/objednavka') {
     appElm.append(OrderPage())
