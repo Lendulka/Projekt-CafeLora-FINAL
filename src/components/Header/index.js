b24b06aeeceb2ebb71216b92a0eab60f9808261e
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-06-25 21:40:20 +0200

    Oprava Objednávka

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,7 +11,7 @@ export const Header = (props) => {
                 <div class="navigation">
                     <button class="nav-btn"></button>
                     <nav class="rollout-nav nav-closed">
-                        <a href="/">domů</a>
+                        <a href="#home">domů</a>
                         <a href="#menu">menu</a>
                         <a href="#gallery">galerie</a>
                         <a href="#contact">kontakt</a>
